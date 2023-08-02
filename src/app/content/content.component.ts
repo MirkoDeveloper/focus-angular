53e45a9798f0011519679b0ce85bc64f67bd883c
MirkoPiccinno <info@mirkodev.it>
2023-08-02 12:12:35 +0200

    prova con rxjs (macchinosissimo.)

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TasksService } from '../tasks.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-content',
@@ -7,5 +9,19 @@ import { Component } from '@angular/core';
 })
 export class ContentComponent {
   title = 'Today';
-  tasknumber = 8;
+  tasknumber: number = this.tasks.totalTasks();
+
+  private tasksSubscription: Subscription = new Subscription();
+
+  constructor(private tasks: TasksService) {
+    this.tasksSubscription = this.tasks.tasksChanged.subscribe(() => {
+      this.tasknumber = this.tasks.totalTasks();
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.tasksSubscription) {
+      this.tasksSubscription.unsubscribe();
+    }
+  }
 }
--- src/app/tasks.service.ts
+++ src/app/tasks.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { TaskInterface } from './interfaces';
 
+import { Subject } from 'rxjs';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -17,6 +19,7 @@ export class TasksService {
   // Metodo per creare un nuovo task
   createTask(task: TaskInterface): void {
     this.tasks.push(task);
+    this.emitTasksChanged();
   }
 
   // Metodo per ottenere la lista delle task
@@ -24,17 +27,29 @@ export class TasksService {
     return this.tasks;
   }
 
+  tasksChanged = new Subject<void>();
+
   // Metodo per aggiornare una specifica task
   updateTask(index: number, updatedTask: TaskInterface): void {
     if (index >= 0 && index < this.tasks.length) {
       this.tasks[index] = updatedTask;
+      this.emitTasksChanged();
     }
   }
-
   // Metodo per eliminare un task
   deleteTask(index: number): void {
     if (index >= 0 && index < this.tasks.length) {
       this.tasks.splice(index, 1);
     }
+  }
+
+  // Notifica cambiamento
+  private emitTasksChanged() {
+    this.tasksChanged.next();
+  }
+
+  // Numero totale dei task
+  totalTasks() {
+    return this.tasks.length;
   }
 }
